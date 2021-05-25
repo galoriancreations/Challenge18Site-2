3a4693439175df950d8e7a92d7e3da81a31c849a
Юра Духно <you@example.com>
2021-05-25 13:34:37 +0300

    add images

---
+++ ting-global-vue-site/src/components/layout/Player.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card__container">
+    <div class="card__body">
+      <div class="image__container">
+        <div class="elementor-widget-container">
+          <figure class="wp-caption">
+            <img :src="card.imgSrc" class="" alt="" loading="eager" />
+            <figcaption class="caption-tittle">
+              {{ card.name }}
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="text__wrapper">
+        <div class="text__container">
+          <p>State: {{ card.state }}</p>
+          <p>School: {{ card.school }}</p>
+          <p>Score: {{ card.score }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+};
+</script>
+
+<style lang="scss">
+@import "../../sass/base.scss";
+.card__container {
+  padding: 37.328px;
+  width: 350px;
+  height: auto;
+  flex-grow: 1;
+}
+// .card__container:last-child {
+//   flex-grow: 10;
+// }
+
+img {
+  width: 100%;
+  object-fit: cover;
+  vertical-align: bottom;
+}
+.wp-caption {
+  margin-bottom: 20px;
+  --font-headings-default: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  --font-base-default: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #333;
+  text-align: center;
+  box-sizing: border-box;
+  transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s,
+    -webkit-border-radius 0.3s, -webkit-box-shadow 0.3s;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 10px;
+}
+.caption-tittle {
+  width: 100%;
+  height: 42px;
+  text-align: center;
+  background-color: rgb(248, 200, 5);
+  font-size: 28px;
+}
+
+p {
+  margin-bottom: 2px;
+  margin-left: 4px;
+}
+</style>
--- ting-global-vue-site/src/views/Players.vue
+++ ting-global-vue-site/src/views/Players.vue
@@ -1,5 +1,84 @@
 <template>
   <Page title="Players" name="players">
-    <WhiteSection></WhiteSection>
+    <WhiteSection>
+      <div class="players__wrap">
+        <Player
+          v-bind:key="card"
+          v-bind:card="card"
+          v-for="card of playersArr"
+        />
+      </div>
+    </WhiteSection>
   </Page>
 </template>
+
+<script>
+import Player from "../components/layout/Player";
+
+export default {
+  components: { Player },
+  data() {
+    return {
+      playersArr: [
+        {
+          imgSrc: "https://i.imgur.com/MIwbEJr.jpg",
+          name: "Nadia Hassona",
+          state: "Israel",
+          school: "Eroni Yud-Bet",
+          score: 1239,
+        },
+        {
+          imgSrc: "https://i.imgur.com/y1AcFjw.jpg",
+          name: "Mariam Abu rhima",
+          state: "Israel",
+          school: "Eroni Yud-Bet",
+          score: 5569,
+        },
+        {
+          imgSrc: "https://i.imgur.com/tgGlgsM.jpg",
+          name: "Fernanda_Matsuoka",
+          state: "Brazil",
+          school: "Brazil - Young Climate Leaders",
+          score: 4523,
+        },
+        {
+          imgSrc: "https://i.imgur.com/l0tyiBX.jpg",
+          name: "Amal Tartir",
+          state: "Nepal",
+          school: "Nepal Premier School",
+          score: 3561,
+        },
+        {
+          imgSrc:
+            "https://i1.wp.com/ting.global/wp-content/uploads/2021/04/1232.jpg?fit=500%2C585&ssl=1",
+          name: "Ashni Rungta",
+          state: "New York",
+          school: "N.Y Professional School",
+          score: 1234,
+        },
+        {
+          imgSrc:
+            "https://i2.wp.com/ting.global/wp-content/uploads/2021/04/Inessa-Kraft-fit.jpg?fit=500%2C585&ssl=1",
+          name: "Inessa Kraft",
+          state: "Portugal",
+          school: "Portugal School",
+          score: 1200,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.players__wrap {
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  align-content: center;
+  align-items: center;
+}
+</style>
