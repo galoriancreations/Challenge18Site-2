49e0b061e3ea08fff21bf8454605864391b19a76
yinonhever <yinonehever@gmail.com>
2021-06-10 05:32:04 +0300

    initial basic UI for join challenge:

--- ting-global-vue-site/src/components/layout/JoinChallenge.vue
+++ ting-global-vue-site/src/components/layout/JoinChallenge.vue
@@ -1,6 +1,26 @@
 <template>
-  <DashboardModal title="Join a Challenge" :active="active">
+  <DashboardModal
+    title="Join a Challenge"
+    :active="active"
+    class="join-challenge"
+  >
     <h3 class="join-challenge__subheading">Choose challenge template</h3>
+    <div class="join-challenge__options">
+      <div
+        class="template-button"
+        v-for="template in templateOptions"
+        :key="template"
+      >
+        <input
+          type="radio"
+          v-model="selection"
+          :value="template"
+          :id="template"
+        />
+        <label :for="template">{{ template }}</label>
+      </div>
+    </div>
+    <BaseButton variant="blue">Join</BaseButton>
   </DashboardModal>
 </template>
 
@@ -12,6 +32,7 @@ export default {
   inject: ["closeModal"],
   data() {
     return {
+      selection: "International SDG",
       templateOptions: ["International SDG", "Family", "Songs & Values"],
     };
   },
@@ -20,3 +41,43 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import "@/sass/base.scss";
+
+.join-challenge {
+  &__subheading {
+    margin-bottom: 3rem;
+  }
+
+  &__options {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+    margin-bottom: 4rem;
+  }
+
+  .button {
+    width: 15rem;
+  }
+}
+
+.template-button {
+  display: block;
+  cursor: pointer;
+
+  input {
+    display: none;
+  }
+
+  label {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
+  input:checked + label {
+    background-color: $color-azure-light;
+  }
+}
+</style>
